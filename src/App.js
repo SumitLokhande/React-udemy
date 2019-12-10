d00d1a186d3b0c5a5f41713e0e24e5844ea8bc39
sumit lokhande <sumit.lokhande@cipherventurecapital.com>
2019-12-10 19:41:20 +0530

    props through attribute done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import Person from './Person/Person'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      <header >
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <Person brand="Yamaha" model="R15"/>
+      <Person brand="Suzuki" model="GSX150"/>
+      <Person brand="Honda" model="CBR150"/>
+
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const person = (props) =>{
+return <p>{props.brand} made {props.model} bike.</p>
+}
+export default person;

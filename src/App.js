c9075f627df59d497d45a84837c8348b9cfc2efa
sumit lokhande <sumit.lokhande@cipherventurecapital.com>
2019-12-10 20:14:42 +0530

    state property used

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import Person from './Person/Person'
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      <header >
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Person brand="Yamaha" model="R15"/>
-      <Person brand="Suzuki" model="GSX150"> with 16BHP</Person>
-      <Person brand="Honda" model="CBR150"/>
-
-    </div>
-  );
+class App extends Component {
+  state ={
+    Bikes:[
+      {brand:"Yamaha", model:"R15"},
+      {brand:"Suzuki", model:"GSX150"},
+      {brand:"Honda", model:"CBR150"}
+    ]
+  };
+  render() {
+    return (
+      <div >
+        <header >
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Person brand={this.state.Bikes[0].brand} model={this.state.Bikes[0].model} />
+        <Person brand={this.state.Bikes[1].brand} model={this.state.Bikes[1].model}> with 16BHP</Person>
+        <Person brand={this.state.Bikes[2].brand} model={this.state.Bikes[2].model}/>
+  
+      </div>
+    );
+  };
+ 
 }
 
 export default App;

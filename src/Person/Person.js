0f1a6d27c4178472f2248dc2198d6b6c02a32732
sumit lokhande <sumit.lokhande@cipherventurecapital.com>
2019-12-11 09:08:16 +0530

    react hook - useState implimented in person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Person.css'
 
-const person = (props) =>{
+const Person = (props) =>{
+   const [listIndex , updateIndexs] = useState({
+        index:[
+            { no :1},
+            { no :2},
+            { no :3},
+        ]
+    });
+    const handler =()=>{
+        updateIndexs({
+            index:[
+                { no :2},
+                { no :2},
+                { no :3},
+            ]
+        })
+    }
 return (
     <div className="card">
-        <p>{props.brand} made {props.model} bike.</p> <p>{props.children}</p>
+<strong>{listIndex.index[0].no}</strong> 
+    <button onClick={handler}>Click</button>
+   <p>{props.brand} made {props.model} bike.</p> <p>{props.children}</p>
     </div>
 )
 }
-export default person;
+export default Person;

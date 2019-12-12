7d19febdb858973934c6636eb87c4b4a44a98267
sumit lokhande <sumit.lokhande@cipherventurecapital.com>
2019-12-12 14:46:46 +0530

    post call on form submit with fetch

--- src/component/Post/porstform.js
+++ src/component/Post/porstform.js
@@ -8,18 +8,36 @@ class porstform extends Component {
             body:''
         }
         this.onChangeHandler=this.onChangeHandler.bind(this)
+        this.onSubmitHandler=this.onSubmitHandler.bind(this)
     }
 
     onChangeHandler(e){
-        console.log(e,"e")
-        console.log(e.target,"target")
+        // console.log(e,"e")/
+        // console.log(e.target,"target")
         this.setState({[e.target.name]:e.target.value})
+    }
+    onSubmitHandler(e){
+        e.preventDefault();
+        console.log('Submit form')
+        const post ={
+            title:this.state.title,
+            body:this.state.body
+        };
+        fetch('https://jsonplaceholder.typicode.com/posts',{
+            method: 'POST',
+            headers:{ 
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res=>res.json())
+        .then(data=> console.log(data))
     }
     render() {
         return (
             <div>
                 <h1>Add Post Form</h1>
-                <form>
+                <form onSubmit={this.onSubmitHandler}> 
                     <div>
                         <label>Title</label>
                         <br/>
@@ -33,7 +51,7 @@ class porstform extends Component {
                     </div>
                     <br/>
 
-                    <button>Submit</button>
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         )

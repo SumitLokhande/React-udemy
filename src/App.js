165af65adfedf012fb8d3fb01298e280dd862b03
sumit lokhande <sumit.lokhande@cipherventurecapital.com>
2019-12-12 16:57:42 +0530

    redux ,react-redux , redux-thunk injected , redux basics began

--- src/App.js
+++ src/App.js
@@ -2,37 +2,40 @@ import React, { Component } from 'react';
 import Person from './component/Person/Person'
 import Post from './component/Post/post'
 import PostForm from './component/Post/porstform'
-
-
+import {Provider} from 'react-redux'
 import './App.css';
+import { createStore, applyMiddleware } from 'redux';
 
+const store = createStore(()=>[],{},applyMiddleware());
 class App extends Component {
-  state ={
-    Bikes:[
-      {brand:"Yamaha", model:"R15"},
-      {brand:"Suzuki", model:"GSX150"},
-      {brand:"Honda", model:"CBR150"}
-    ]
-  };
-  modelNameHandler=(newBike)=>{
-    this.setState({
-      Bikes:[
-        {brand:"Yamaha", model:newBike},
-        {brand:"Suzuki", model:"GSX150"},
-        {brand:"KTM", model:"RC200"}
-      ]
-    })
-  }
+  // state ={
+  //   Bikes:[
+  //     {brand:"Yamaha", model:"R15"},
+  //     {brand:"Suzuki", model:"GSX150"},
+  //     {brand:"Honda", model:"CBR150"}
+  //   ]
+  // };
+  // modelNameHandler=(newBike)=>{
+  //   this.setState({
+  //     Bikes:[
+  //       {brand:"Yamaha", model:newBike},
+  //       {brand:"Suzuki", model:"GSX150"},
+  //       {brand:"KTM", model:"RC200"}
+  //     ]
+  //   })
+  // }
   render() {
     return (
-      <div className="content">
-        <button onClick={this.modelNameHandler.bind(this,'R3')}>Change Model</button>
+     <Provider store={store}>
+        <div className="content">
+        {/* <button onClick={this.modelNameHandler.bind(this,'R3')}>Change Model</button>
         <Person brand={this.state.Bikes[0].brand} model={this.state.Bikes[0].model} />
         <Person brand={this.state.Bikes[1].brand} model={this.state.Bikes[1].model}> with 16BHP</Person>
-        <Person brand={this.state.Bikes[2].brand} model={this.state.Bikes[2].model}/>
+        <Person brand={this.state.Bikes[2].brand} model={this.state.Bikes[2].model}/> */}
         {/* <Post/> */}
         <PostForm/>
       </div>
+     </Provider>
     );
   };
  

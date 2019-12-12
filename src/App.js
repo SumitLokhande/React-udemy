48e5f9d8fcd7ee5eadc237f566bdfeee3d667d77
sumit lokhande <sumit.lokhande@cipherventurecapital.com>
2019-12-12 11:49:55 +0530

    form creation , form binding done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Person from './component/Person/Person'
 import Post from './component/Post/post'
+import PostForm from './component/Post/porstform'
+
 
 import './App.css';
 
@@ -28,7 +30,8 @@ class App extends Component {
         <Person brand={this.state.Bikes[0].brand} model={this.state.Bikes[0].model} />
         <Person brand={this.state.Bikes[1].brand} model={this.state.Bikes[1].model}> with 16BHP</Person>
         <Person brand={this.state.Bikes[2].brand} model={this.state.Bikes[2].model}/>
-        <Post></Post>
+        {/* <Post/> */}
+        <PostForm/>
       </div>
     );
   };
--- src/component/Post/porstform.js
+++ src/component/Post/porstform.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 
 class porstform extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            title:'',
+            body:''
+        }
+        this.onChangeHandler=this.onChangeHandler.bind(this)
+    }
+
+    onChangeHandler(e){
+        console.log(e,"e")
+        console.log(e.target,"target")
+        this.setState({[e.target.name]:e.target.value})
+    }
     render() {
         return (
             <div>
-                
+                <h1>Add Post Form</h1>
+                <form>
+                    <div>
+                        <label>Title</label>
+                        <br/>
+                        <input type="text" name="title" onChange={this.onChangeHandler}  value={this.state.title}/>
+                    </div>
+                    <br/>
+                    <div>
+                        <label>Body</label>
+                        <br/>
+                        <textarea type="text" name="body" onChange={this.onChangeHandler} value={this.state.body} />
+                    </div>
+                    <br/>
+
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
